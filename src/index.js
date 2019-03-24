f3d3323826442322c484306c09eefe07624eacf5
verymadmax <madmax.verymad@gmail.com>
2019-03-24 19:09:09 +0300

    feat: new solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var strArr = str.split('');
+  var brack = bracketsConfig;
+  for (var i=0;i<strArr.length;i++){
+    for (var j = 0; j < brack.length; j++){
+      if(strArr[i] === brack[j][0] && strArr[i+1] === brack[j][1]){
+        strArr.splice(i,2);
+        if (strArr.length == 0){
+        return true;
+        }
+        i = -1;
+      }
+    }
+  }
+  return false;
 }
